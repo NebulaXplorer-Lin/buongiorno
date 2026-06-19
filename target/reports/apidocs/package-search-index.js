cfea0c9ec6bcf62fabef0d2bd68f654816bfcb73
NebulaXplorer-Lin <linzhehan666@gmail.com>
2026-06-19 16:59:35 +0800

    clean

--- target/reports/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"app"},{"l":"model"},{"l":"persistence"},{"l":"service"},{"l":"ui.javafx"},{"l":"ui.javafx.controller"},{"l":"util"}];updateSearchResults();
